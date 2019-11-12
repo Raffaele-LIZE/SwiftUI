0be6e664f3d28fdd03f0e8559c323b63655d959b
Raffaële <raffaele.lize@project-factory.eu>
2019-11-12 12:41:04 +0100

    feat(*): section 1 done

---
+++ combining_views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  combining_views
+//
+//  Created by Raffaële PY  on 12/11/2019.
+//  Copyright © 2019 Raffaële PY . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Rdev will be the best mobile developer")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
