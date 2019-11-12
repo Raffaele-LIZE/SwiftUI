0aeaa0657c55b6b5c8933442a6d0a8e7543b3097
Raffaële <raffaele.lize@project-factory.eu>
2019-11-12 15:21:50 +0100

    feat(*): section 4 done

---
+++ combining_views/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  combining_views
+//
+//  Created by Raffaële PY  on 12/11/2019.
+//  Copyright © 2019 Raffaële PY . All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
